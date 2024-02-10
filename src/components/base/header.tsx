3fc40a2edb1f5494af5074873194ff12208d11d3
kinton <kintonk@yandex.ru>
2024-02-10 23:08:22 +0300

    Home page layout

---
+++ src/components/base/header.tsx
@@ -0,0 +1,33 @@
+import { env } from "@/env";
+import { Button } from "@/components/ui/button-base";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+
+export default function Header() {
+
+  return (
+    <header className="container bg-white">
+      <nav
+        className="mx-auto flex flex-col md:flex-row max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="sm:text-center">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">{env.NEXT_PUBLIC_PROJECT_NAME}</span>
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="gap-2 flex flex-1 justify-end">
+          <Link href={`mailto:${env.NEXT_PUBLIC_SUPPORT_EMAIL}`}>
+            <Button>Написать в поддержку</Button>
+          </Link>
+          <Button onClick={() => signIn()}>Войти</Button>
+        </div>
+      </nav>
+    </header>
+  );
+}
