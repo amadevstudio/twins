e7150969c42f751350965b3a2ab7af84a9861393
kinton <kintonk@yandex.ru>
2024-02-19 14:05:25 +0300

    Add link to / for logo

--- src/components/base/header.tsx
+++ src/components/base/header.tsx
@@ -12,17 +12,15 @@ export default function Header() {
         className="mx-auto flex max-w-7xl flex-col items-center justify-between p-6 md:flex-row lg:px-8"
         aria-label="Global"
       >
-        <div className="sm:text-center">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">{env.NEXT_PUBLIC_PROJECT_NAME}</span>
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
-            />
-          </a>
-        </div>
-        <div className="flex flex-1 justify-center md:justify-end max-w-full flex-wrap gap-2">
+        <Link href="/" className="sm:text-center">
+          <span className="sr-only">{env.NEXT_PUBLIC_PROJECT_NAME}</span>
+          <img
+            className="h-8 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt=""
+          />
+        </Link>
+        <div className="flex max-w-full flex-1 flex-wrap justify-center gap-2 md:justify-end">
           {!!sessionData && (
             <>
               <Link href="/">
@@ -36,7 +34,11 @@ export default function Header() {
           <Link href={`mailto:${env.NEXT_PUBLIC_SUPPORT_EMAIL}`}>
             <Button>Написать в поддержку</Button>
           </Link>
-          {!sessionData && <Button onClick={() => signIn(undefined, { callbackUrl: '/user' })}>Войти</Button>}
+          {!sessionData && (
+            <Button onClick={() => signIn(undefined, { callbackUrl: "/user" })}>
+              Войти
+            </Button>
+          )}
           {!!sessionData && <Button onClick={() => signOut()}>Выйти</Button>}
         </div>
       </nav>
