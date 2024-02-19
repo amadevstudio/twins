8ee2248df2553ff2894c98aa326bcaaff795ce4d
kinton <kintonk@yandex.ru>
2024-02-19 13:43:51 +0300

    Enable logging for production

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,22 +2,23 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 import { env } from "@/env";
 import { appRouter } from "@/server/api/root";
-import {createContextInner, createTRPCContext} from "@/server/api/trpc";
-import {createServerSideHelpers} from "@trpc/react-query/server";
+import { createContextInner, createTRPCContext } from "@/server/api/trpc";
+import { createServerSideHelpers } from "@trpc/react-query/server";
 import superjson from "superjson";
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-          );
-        }
-      : undefined,
+  onError: ({ path, error }) => {
+    if (env.NODE_ENV === "development") {
+      console.error(
+        `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+      );
+    } else {
+      console.error(error);
+    }
+  },
 });
 
 export const serverSideHelper = createServerSideHelpers({
