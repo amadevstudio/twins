ce1ea0ea5d25925555200649488e3cea9370829e
kinton <kintonk@yandex.ru>
2024-02-27 00:10:47 +0300

    Separate words

--- src/server/service/keyWord.ts
+++ src/server/service/keyWord.ts
@@ -1,6 +1,7 @@
 export function processKeyWordsString(keyWordsString: string) {
   return keyWordsString
     .replace(/^\s+|\s+$/g, "") // Remove spaces from start and end
+    .replace(/[^а-яА-Яa-zA-Z0-9 ]/g, "") // Remove all but not in range
     .split(" ")
     .filter((kw) => kw.length > 0)
     .map((kw) => kw.toLowerCase());
