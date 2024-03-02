0c27b7edb2d658e9e5c91dc93739f4b20b8671fc
kinton <kintonk@yandex.ru>
2024-03-02 16:51:25 +0300

    Fix redis_conf

--- app/src/jobs/redis_conf.ts
+++ app/src/jobs/redis_conf.ts
@@ -1,6 +1,6 @@
 import { env } from "@/env";
 
 export const config: { host: string; password: string } = {
-  host: env.REDIS_SERVER as string,
-  password: env.REDIS_PASSWORD as string,
+  host: env.REDIS_SERVER,
+  password: env.REDIS_PASSWORD,
 };
